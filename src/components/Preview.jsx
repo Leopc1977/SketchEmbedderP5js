e7f94cf4611f450846d36a37db421213288293a9
Leopc1977 <46353973+Leopc1977@users.noreply.github.com>
2025-03-21 00:18:56 +0100

    build(snippetCodes): try fix prod paths

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -84,7 +84,7 @@ function Preview(props) {
         const filesName = ["a.js", "b.js", "c.js"];
 
         filesName.forEach((fileName) => {
-            const path = `../snippetsCode/${fileName}`;
+            const path = import.meta.env.PROD ? `/snippetsCode/${fileName}` : `../snippetsCode/${fileName}`;
             import(path).then((module) => {
                 setSketchList((prev) => [...prev,
                     new Sketch(
